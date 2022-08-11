38a1d7e206458387433fde33971c8249d22ff5f7
dunglq <dunglq@bkav.com>
2022-08-11 17:01:38 +0700

    Cap nhat xoa file src_origin

--- note-app/src_origin/redux/selectors.js
+++
@@ -1,9 +0,0 @@
-import {createSelector} from 'reselect'
-export const todoListSelector = state => state.noteList
-export const searchSelector = state => state.search.search
-
-export const todosRemainingSelector = createSelector(todoListSelector, searchSelector, (todoLists, searchText) => {
-   return todoLists.filter(todoList => {
-        return (todoList.text.includes(searchText))
-    })
-}) 
